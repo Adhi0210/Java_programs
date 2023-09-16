4c9f0f0a6802ee5faa38a70fee09a59004f268c3
M.B.prasad <144529304+Adhi0210@users.noreply.github.com>
2023-09-16 15:23:27 +0530

    Add files via upload

---
+++ Year.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+public class Year {
+public static void main(String[] args) {
+int month = 10;
+switch (month) {
+case 1:
+System.out.println("January");
+break;
+case 2:
+System.out.println("Febaury");
+break;
+case 3:
+System.out.println("March");
+break;
+case 4:
+System.out.println("Aprial");
+break;
+case 5:
+System.out.println("May");
+break;
+case 6:
+System.out.println("June");
+break;
+case 7:
+System.out.println("July");
+break;
+case 8:
+System.out.println("August");
+break;
+case 9:
+System.out.println("September");
+break;
+case 10:
+System.out.println("October");
+break;
+case 11:
+System.out.println("November");
+break;
+case 12:
+System.out.println("December");
+break;
+}
+}
+}
