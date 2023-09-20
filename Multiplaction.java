02286025dc40a9dab6ab3f8fdac01148a3befaf7
M.B.prasad <144529304+Adhi0210@users.noreply.github.com>
2023-09-20 09:40:02 +0530

    Add files via upload

---
+++ Multiplaction.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Multiplication {
+    public static void main(String args[]) {
+Scanner Sc = new Scanner(System.in);
+System.out.println("enter the first integer");
+int i1 = Sc.nextInt();
+System.out.println("enter second integer");
+int i2 = Sc.nextInt();
+int i3=i1*i2;
+System.out.println("Mul of"+i1+"and"+i2+"is"+i3);
+}
+}
